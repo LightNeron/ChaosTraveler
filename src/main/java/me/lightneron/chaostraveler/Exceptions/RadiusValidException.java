69f496bc846a98658641818d065e349832174fc8
LightNeron <alex_miner2021@mail.ru>
2024-03-10 20:31:58 +0300

    Добавлено исключение RadiusValidException

---
+++ src/main/java/me/lightneron/chaostraveler/Exceptions/RadiusValidException.java
@@ -0,0 +1,14 @@
+package me.lightneron.chaostraveler.Exceptions;
+
+import me.lightneron.chaostraveler.ChaosTraveler;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class RadiusValidException extends Exception{
+    public RadiusValidException() {
+        JavaPlugin.getPlugin(ChaosTraveler.class).getLogger().warning("Минимальный радиус не может быть больше чем максимальный");
+        JavaPlugin.getPlugin(ChaosTraveler.class).getLogger().warning("Поэтому минимальный и максимальный радиус был сброшен до дефолтных значений");
+        JavaPlugin.getPlugin(ChaosTraveler.class).getConfig().set("NormalTeleport.RadiusMin", 3000);
+        JavaPlugin.getPlugin(ChaosTraveler.class).getConfig().set("NormalTeleport.RadiusMax", 3000);
+        JavaPlugin.getPlugin(ChaosTraveler.class).saveConfig();
+    }
+}
