2fc6cdd6c10903497c4eeb1055e39bd82fc0609a
LightNeron <alex_miner2021@mail.ru>
2024-03-10 18:26:53 +0300

    Добавлен ItemStack для меню

---
+++ src/main/java/me/lightneron/chaostraveler/ItemStacks/ItemStacks.java
@@ -0,0 +1,40 @@
+package me.lightneron.chaostraveler.ItemStacks;
+
+import me.lightneron.chaostraveler.ChaosTraveler;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.ArrayList;
+
+public class ItemStacks {
+    public static ItemStack NormalTeleport(Player player) {
+        ArrayList<Component> Lore = new ArrayList<>();
+
+
+        if (JavaPlugin.getPlugin(ChaosTraveler.class).getConfig().getBoolean("NormalTeleport.ShowRadiusInMenu")) {
+            Lore.add(MiniMessage.miniMessage().deserialize("<#ffff55>Радиус:"));
+            Lore.add(MiniMessage.miniMessage().deserialize(" <#ffed4f>Минимальный: <#4a65ff>" + JavaPlugin.getPlugin(ChaosTraveler.class).getConfig().getInt("NormalTeleport.RadiusMin")));
+            Lore.add(MiniMessage.miniMessage().deserialize(" <#ffed4f>Максимальный: <#4a65ff>" + JavaPlugin.getPlugin(ChaosTraveler.class).getConfig().getInt("NormalTeleport.RadiusMax")));
+        }
+
+        if (player.hasPermission("ChaosTraveler.NormalTeleport")) {
+            Lore.add(MiniMessage.miniMessage().deserialize("<#36fff2>Разрешено: " + "<#0dff00>Да"));
+        } else {
+            Lore.add(MiniMessage.miniMessage().deserialize("<#36fff2>Разрешено: " + "<#ff0d0d>Нет"));
+        }
+
+
+        ItemStack NormalTeleport = new ItemStack(Material.TORCH);
+        ItemMeta NormalTeleportMeta = NormalTeleport.getItemMeta();
+        NormalTeleportMeta.displayName(MiniMessage.miniMessage().deserialize("<#55ffff>Обычная телепортация"));
+        NormalTeleportMeta.lore(Lore);
+
+        NormalTeleport.setItemMeta(NormalTeleportMeta);
+        return NormalTeleport;
+    }
+}
