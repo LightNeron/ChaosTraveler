9fb491fd0e95679ee6e9197faf01555c678a37e9
BuildTools <unconfigured@null.spigotmc.org>
2024-03-10 11:56:20 +0300

    New

---
+++ src/main/java/me/lightneron/chaostraveler/ChaosTraveler.java
@@ -0,0 +1,17 @@
+package me.lightneron.chaostraveler;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ChaosTraveler extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
